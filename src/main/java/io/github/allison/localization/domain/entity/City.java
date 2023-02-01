087f058e389086581108f2377b49e3f652280c8a
allisonvinicius <allisonvinicius63@gmail.com>
2023-02-01 17:15:54 -0400

    feat: created class entty and repository jpa

---
+++ src/main/java/io/github/allison/localization/domain/entity/City.java
@@ -0,0 +1,31 @@
+
+package io.github.allison.localization.domain.entity;
+
+
+        import jakarta.persistence.Column;
+        import jakarta.persistence.Entity;
+        import jakarta.persistence.Id;
+        import jakarta.persistence.Table;
+        import lombok.AllArgsConstructor;
+        import lombok.Data;
+        import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tb_city")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class City {
+
+    @Id
+    @Column(name = "id_cidade")
+    private Long id;
+    @Column(name = "name", length = 50)
+    private String name;
+    @Column(name = "qty_population")
+    private  long population;
+
+
+
+}
---
+++ src/main/java/io/github/allison/localization/domain/repository/CityRepository.java
@@ -0,0 +1,9 @@
+package io.github.allison.localization.domain.repository;
+
+import io.github.allison.localization.domain.entity.City;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CityRepository extends JpaRepository<City,Long> {
+
+
+}
