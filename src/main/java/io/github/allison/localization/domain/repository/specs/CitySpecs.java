aa37a285f90f2c55f2c89e90edad6c0965182687
allisonvinicius <allisonvinicius63@gmail.com>
2023-02-13 23:53:38 -0400

    feat: include speficication in application

---
+++ src/main/java/io/github/allison/localization/domain/repository/specs/CitySpecs.java
@@ -0,0 +1,15 @@
+package io.github.allison.localization.domain.repository.specs;
+
+import io.github.allison.localization.domain.entity.City;
+import org.springframework.data.jpa.domain.Specification;
+
+public class CitySpecs {
+
+    public static Specification<City> nameEqual(String name){
+        return (root, query, cb) -> cb.equal(root.get("name"), name);
+    }
+
+    public static Specification<City> populationGreaterThan(Integer value){
+        return (root, query, cb) -> cb.greaterThan(root.get("population"), value);
+    }
+}
